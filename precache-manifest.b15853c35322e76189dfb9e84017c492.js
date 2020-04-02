88e420336f4a6c859cb25b832d495219fc454011
Rachael A. Concessio <RachaelConcessio@gmail.com>
2020-04-02 10:19:19 -0400

    Updates

---
+++ precache-manifest.b15853c35322e76189dfb9e84017c492.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beff05af41ebca39045584038d48fdd9",
+    "url": "/games-db/index.html"
+  },
+  {
+    "revision": "aca30224583097333f1b",
+    "url": "/games-db/static/css/main.fc188863.chunk.css"
+  },
+  {
+    "revision": "099a835f89f423c6a8e3",
+    "url": "/games-db/static/js/2.8cce5f37.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/games-db/static/js/2.8cce5f37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aca30224583097333f1b",
+    "url": "/games-db/static/js/main.7ee3988b.chunk.js"
+  },
+  {
+    "revision": "050778235565add6cd41",
+    "url": "/games-db/static/js/runtime-main.f4c585fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/games-db/static/media/logo.5d5d9eef.svg"
+  }
+]);
